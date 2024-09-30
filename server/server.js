4fa5fc26e15298f95a5861d15c8c13933432eccd
hassan <ec221016@qmul.ac.uk>
2024-09-30 23:10:53 +0100

    trying to fix deployment issues prt 2

--- server/server.js
+++ server/server.js
@@ -107,10 +107,10 @@ app.post('/clear', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
 
